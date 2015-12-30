c8dfb9e8df769d78c4fc8778bdd2cd0c7b575f1d
unknown <mahak.bhui@HDC3-D-451R23G.dir.svc.accenture.com>
2015-12-30 15:14:45 +0530

    pushing

---
+++ ProblemSolving/src/ProblemSolving/DuplicateWords.java
@@ -0,0 +1,40 @@
+package probelmSolving;
+
+import java.util.HashMap;
+import java.util.Set;
+
+public class DuplicateWords {
+
+	public static void main(String[] args) {
+
+		String sentence = "Super Man ";
+		HashMap<String, Integer> map = new HashMap<>();
+		String[] arr = sentence.split(" ");
+
+		for (String word : arr) {
+			word = word.toLowerCase();
+			if (map.containsKey(word)) {
+				int count = map.get(word);
+				map.put(word, count + 1);
+			} else {
+				map.put(word, 1);
+			}
+		}
+
+		Set<String> keys = map.keySet();
+		boolean foundDuplicate = false;
+		for (String key : keys) {
+			int count = map.get(key);
+			if (count > 1) {
+				foundDuplicate = true;
+				System.out.println(key + ": " + count);
+			}
+
+		}
+		if(!foundDuplicate){
+		System.out.println("No duplicate words!");	
+		}
+
+	}
+
+}
